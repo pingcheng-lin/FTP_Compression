4c26aab60f27ef88baab0a223348228313703846
Ben <pcbenlin@gmail.com>
2021-04-01 00:04:49 +0800

    multiple send done

--- Client/client.c
+++ Client/client.c
@@ -4,97 +4,119 @@
 #include <unistd.h> //read, write
 #include <arpa/inet.h> //inet_addr
 #include <string.h> //strcmp
-void input_link(struct sockaddr_in *srv);
-int input();
+#include <time.h> //current time
+#define MAX_FILENAME_SIZE 30
+int input(struct sockaddr_in *srv, char *filename);
 void sendd(char *file);
 
 int main() {
-    int fd; /* socket descriptor */
-    struct sockaddr_in srv; /* used by connect() */
-    char buf[512]; /* used by write() */
-    int nbytes; /* used by write() */
-    int ing = 1;
-    printf("Welcome\nYou can 'link' [an IP address] [a port], 'send' [a file], or 'leave'.\n===\n");
-    
-    
-    /* Get the file descriptor */
+    int fd; //socket descriptor
+    struct sockaddr_in srv; //used by connect()
+    char buf[512]; //used by write()
+    int nbytes; //used by write()
+    printf("Welcome\nYou can 'link' [an IP address] [a port], 'send' [a file], or 'leave'.\n");
+    printf("===\nWaiting...\n");
+
+    //Get the file descriptor
     fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(fd < 0) {
         printf("Error");
         exit(1);
     }
     
-    
-    /* Connect the file descriptor to the server’s IP and port */
-    srv.sin_family = AF_INET;
-    input_link(&srv);
-    if(connect(fd, (struct sockaddr*) &srv, sizeof(srv)) < 0) {
-        perror("connect"); 
-        exit(1);
-    }
-    printf("The server with IP address \"%s\" has accepted your connection.\n", inet_ntoa(srv.sin_addr));
-    
-
-    /* Write data to the socket */
-    char filename[30];
-    input(filename);
-    FILE *file = fopen(filename, "r");
-    int num, total = 0;
-    if((nbytes = write(fd, filename, sizeof(buf))) < 0) { //send filename
-        perror("write");
-        exit(1);
-    }
-    fseek(file, 0 , SEEK_END);
-    int filesize = ftell(file);
-    fseek(file, 0 , SEEK_SET);
-    printf("%d\n", filesize);
-    if((nbytes = write(fd, &filesize, sizeof(filesize))) < 0) { //send filesize
-        perror("write");
-        exit(1);
-    }
-    while(!feof(file)) { //send file
-        num = fread(buf, sizeof(char), sizeof(buf), file);
-        total += num;
-        if((nbytes = write(fd, buf, num)) < 0) {
-            perror("write");
-            exit(1);
+    while(1) {
+        char filename[MAX_FILENAME_SIZE];
+        int flag = input(&srv, filename); //1: link, 2: send, 3:leave
+        if(flag == 1) {
+            //Connect the file descriptor to the server’s IP and port
+            srv.sin_family = AF_INET;
+            if(connect(fd, (struct sockaddr*) &srv, sizeof(srv)) < 0) {
+                perror("connect"); 
+                exit(1);
+            }
+            printf("The server with IP address \"%s\" has accepted your connection.\n", inet_ntoa(srv.sin_addr));
+            printf("===\nwaiting...\n");
+        }
+        if(flag == 2) {
+            //Write data to the socket
+            FILE *file;
+            int num, total = 0;
+            //send flag
+            if((nbytes = write(fd, "send", sizeof(buf))) < 0) { 
+                perror("write");
+                exit(1);
+            }
+            //open file
+            if((file = fopen(filename, "r")) == NULL) {
+                if((nbytes = write(fd, "error", sizeof(buf))) < 0) { 
+                    perror("write");
+                    exit(1);
+                }
+                perror("fopen");
+                exit(1);
+            }
+            
+            //send filename
+            if((nbytes = write(fd, filename, sizeof(buf))) < 0) { 
+                perror("write");
+                exit(1);
+            }
+            //send filesize
+            fseek(file, 0 , SEEK_END);
+            int filesize = ftell(file);
+            fseek(file, 0 , SEEK_SET);
+            if((nbytes = write(fd, &filesize, sizeof(filesize))) < 0) { 
+                perror("write");
+                exit(1);
+            }
+            //send file
+            while(!feof(file)) {
+                num = fread(buf, sizeof(char), sizeof(buf), file);
+                total += num;
+                printf("%d\n", num);
+                if((nbytes = write(fd, buf, num)) < 0) {
+                    perror("write");
+                    exit(1);
+                }
+            }
+            printf("Original file length: %d bytes, compressed file length: %d bytes (ratio:)\n", filesize, total);
+            time_t t = time(NULL);
+            struct tm time = *localtime(&t);
+            printf("Time to upload: %d/%02d/%02d %02d:%02d\n", time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, time.tm_hour, time.tm_min);
+            printf("Using fixed-length codeword (3 bits)\n");
+            printf("===\nwaiting...\n");
+        }
+        if(flag == 3){
+            if((nbytes = write(fd, "leave", sizeof(buf))) < 0) { 
+                perror("write");
+                exit(1);
+            }
+            printf("Bye bye.\n");
+            break;
         }
     }
-    printf("Original file length: %d bytes, compressed file length: %d bytes (ratio:)\n", filesize, total); 
-    printf("Time to upload:\n");
-    printf("Using fixed-length codeword (3 bits)\n");
-    printf("Bye bye.\n");
     return 0;
 }
 
-void input_link(struct sockaddr_in *srv) {
-    char command[5];
-    char ip_address[15];
+int input(struct sockaddr_in *srv, char *filename) {
+    char command[5], ip_address[15];
     int port;
-    scanf("%s", command);
-    while(strcmp("link", command) != 0) {
-        scanf("%s", command);
-    }
-    scanf("%s", ip_address);
-    scanf("%d", &port);
-    srv->sin_port = htons(port); /* connect: socket 'fd' to port 80 */
-    srv->sin_addr.s_addr = inet_addr(ip_address); /* connect: connect to IP address “140.117.11.87” */
-}
-int input(char *filename) {
-    char command[5];
-    scanf("%s", command);
     while(1) {
-        if(strcmp("send", command) == 0) {
-            scanf("%s", filename);
+        scanf("%s", command);
+        if(strcmp("link", command) == 0) {
+            scanf("%s", ip_address);
+            srv->sin_addr.s_addr = inet_addr(ip_address); //connect: connect to IP address
+            scanf("%d", &port);
+            srv->sin_port = htons(port); //connect: socket 'fd' to port
             return 1;
         }
+        else if(strcmp("send", command) == 0) {
+            scanf("%s", filename);
+            return 2;
+        }
         else if(strcmp("leave", command) == 0)
-            return 0;
+            return 3;
         else
-            printf("Need 'link' [an IP address] [a port], 'send' [a file], or 'leave'.");
+            printf("Need 'link' [an IP address] [a port], 'send' [a file], or 'leave'.\n");
     }
-    
-}
-void sendd(char *file) {
-    printf("1\n");
 }
--- Server/server.c
+++ Server/server.c
@@ -3,35 +3,36 @@
 #include <stdlib.h> //exit
 #include <unistd.h> //read, write
 #include <arpa/inet.h> //inet_ntoa
+#include <string.h> //strcpy
+#define MAX_FILENAME_SIZE 30
 int main() {
     int fd;
-    struct sockaddr_in srv; /* used by bind() */
-    struct sockaddr_in cli; /* used by accept() */
-    int newfd; /* returned by accept() */
-    int cli_len = sizeof(cli); /* used by accept() */
-    char buf[512]; /* used by read() */
-    int nbytes = 0; /* used by read() */
+    struct sockaddr_in srv; //used by bind()
+    struct sockaddr_in cli; //used by accept()
+    int newfd; //returned by accept()
+    int cli_len = sizeof(cli); //used by accept()
+    char buf[512]; //used by read()
+    int nbytes = 0; //used by read()
 
     
-    /* Get the file descriptor */
+    //Get the file descriptor
     fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(fd < 0) {
         printf("Error");
-       exit(1);
+        exit(1);
     }
 
 
-    srv.sin_family = AF_INET; /* use the Internet address family */
-    srv.sin_port = htons(1234); /* bind socket 'fd' to port 80 */
+    srv.sin_family = AF_INET; //use the Internet address family
+    srv.sin_port = htons(1234); //bind socket 'fd' to port 80
     srv.sin_addr.s_addr = htonl(INADDR_ANY);
-    /* Connect the file descriptor with the IP address and port */
+    //Connect the file descriptor with the IP address and port
     if(bind(fd, (struct sockaddr*) &srv, sizeof(srv)) < 0) {
         perror("bind"); 
         exit(1);
     }
     
-    
-    /* Wait for requests from clients */
+    //Wait for requests from clients
     if(listen(fd, 5) < 0) {
         perror("listen");
         exit(1);
@@ -40,44 +41,67 @@ int main() {
         printf("Listening...\n");
     }
     
-    
-    /* Accept requests */
+    //Accept requests
     newfd = accept(fd, (struct sockaddr*) &cli, &cli_len);
     if(newfd < 0) {
         perror("accept");
         exit(1);
     }
-    printf("A client \"%s\" has connected via port num %d using SOCK_STREAM (TCP)\n", inet_ntoa(cli.sin_addr), ntohs(srv.sin_port));
+    printf("A client \"%s\" has connected via port num %d using SOCK_STREAM (TCP)\n", inet_ntoa(cli.sin_addr), ntohs(cli.sin_port));
     
-    
-    /* Read data from the socket */
-    FILE *file;
-    if(read(newfd, buf, sizeof(buf)) < 0) { //read filename
-        perror("read");
-        exit(1);
-    }
-    else {
-        printf("%s\n", buf);
-        file = fopen(buf, "w");//reset file
-        fclose(file);
-        file = fopen(buf, "a");//append file
-    }
-    int filesize;
-    if(read(newfd, &filesize, sizeof(filesize)) < 0) { //read filesize
-        perror("read");
-        exit(1);
-    }
-    printf("filesize:%d\n", filesize);
-    for(int i = filesize; i > 0; i-=nbytes)
-        if((nbytes = read(newfd, buf, sizeof(buf))) < 0) { //read file
+    while(1) {
+        //Read data from the socket
+        FILE *file;
+        char filename[MAX_FILENAME_SIZE];
+        int filesize;
+        //read flag
+        if(read(newfd, buf, sizeof(buf)) < 0) { 
             perror("read");
             exit(1);
         }
-        else {
-            fwrite(buf, sizeof(char), nbytes, file);
+        else if(strcmp(buf, "leave") == 0) {
+            printf("The client \"%s\" with port %d has terminated the connection.\n", inet_ntoa(cli.sin_addr), ntohs(cli.sin_port));
+            break;
         }
-    printf("The client sends a file \"1.txt\" with size of %d bytes.\n", nbytes);
-    printf("The Huffman coding data are stored in \"1-code.txt\".\n");
-    printf("The client \"%s\" with port %d has terminated the connection.\n", inet_ntoa(cli.sin_addr), ntohs(srv.sin_port));
+        //read filename
+        if(read(newfd, buf, sizeof(buf)) < 0) { 
+            perror("read");
+            exit(1);
+        }
+        else if(strcmp(buf, "error") == 0) {
+            printf("File open error!!!\n");
+            exit(1);
+        }
+        else {
+            strcpy(filename, buf);
+            printf("%s\n", filename);
+            file = fopen(filename, "w");//reset file
+            fclose(file);
+            file = fopen(filename, "a");//append file
+        }
+        //read filesize
+        if(read(newfd, &filesize, sizeof(filesize)) < 0) { 
+            perror("read");
+            exit(1);
+        }
+        printf("filesize:%d\n", filesize);
+        //read file
+        for(int i = filesize; i > 0; i-=nbytes)
+            //printf("%d=%d\n", i, nbytes);
+            if((nbytes = read(newfd, buf, sizeof(buf))) < 0) { 
+                perror("read");
+                exit(1);
+            }
+            else{
+                fwrite(buf, sizeof(char), nbytes, file);
+                memset(buf, 0, 512*sizeof(buf[0]));
+            }
+                
+        printf("The client sends a file \"%s\" with size of %d bytes.\n", filename, filesize);
+        printf("The Huffman coding data are stored in \"txt\".\n");
+        
+        printf("===\n");
+        fclose(file);
+    }
     return 0;
 }
